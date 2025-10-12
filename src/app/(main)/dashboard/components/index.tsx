be77cddf0ac6567434eca11eb19df3c2c98f72dc
Ayush Kamboj <aayushkamboj7@gmail.com>
2025-10-13 01:01:14 +0530

    Add:Profile Completion banner dir rename

--- src/app/(main)/dashboard/components/index.tsx
+++ src/app/(main)/dashboard/components/index.tsx
@@ -1,11 +1,11 @@
-import ProfileCompletionBanner from "../../components/ProfileCompletionBanner";
+import ProfileCompletionBanner from "../../components/Profile-Completion-Banner";
 import ExactlyWhatYouNeedBanner from "../../components/What-you-need-banner";
 
 const DashboardPage = () => {
   return (
     <>
       <div className="h-[calc(100vh-70px)] p-6 overflow-y-scroll bg-[#f5f5f7]">
-        <div className="flex gap-6 justify-between">
+        <div className="flex gap-4 justify-between">
           <ExactlyWhatYouNeedBanner />
           <ProfileCompletionBanner />
         </div>
