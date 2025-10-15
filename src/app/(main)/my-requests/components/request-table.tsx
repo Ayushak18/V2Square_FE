44b5b7e4d3b4f5f2c2f5c95fce57ae309c7aae4a
Ayush Kamboj <aayushkamboj7@gmail.com>
2025-10-15 15:34:22 +0530

    :Add:Search Bar in My request page

--- src/app/(main)/my-requests/components/request-table.tsx
+++ src/app/(main)/my-requests/components/request-table.tsx
@@ -1,4 +1,5 @@
 // components/StatusTable.tsx
+import { EllipsisVertical, Search } from "lucide-react";
 import React from "react";
 
 type Status = "Open" | "Draft" | "Pending" | "Closed";
@@ -32,55 +33,75 @@ const statusClass = (status: Status) => {
 };
 
 const StatusTable: React.FC<StatusTableProps> = ({ data }) => (
-  <div className="overflow-x-auto my-4 border border-[#e7e0ec] rounded-2xl">
-    <table className="min-w-full bg-white rounded-xl">
-      <thead>
-        <tr>
-          <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c] rounded-tl-xl">
-            Product/Service
-          </th>
-          <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c]">
-            Quantity
-          </th>
-          <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c]">
-            Budget Range
-          </th>
-          <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c]">
-            Status
-          </th>
-          <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c]">
-            Pitches
-          </th>
-          <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c] rounded-tr-xl">
-            Posted On
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((row, idx) => (
-          <tr
-            key={idx}
-            className="border border-gray-100 hover:bg-[#f5f5f5] transition"
-          >
-            <td className="py-4 px-6 text-center text-sm cursor-pointer">{row.productService}</td>
-            <td className="py-4 px-6 text-center text-sm cursor-pointer">{row.quantity}</td>
-            <td className="py-4 px-6 text-center text-sm cursor-pointer">{row.budgetRange}</td>
-            <td className="py-4 px-6 text-center text-sm cursor-pointer">
-              <span
-                className={`inline-block px-4 py-1 rounded-full text-sm cursor-pointer font-medium ${statusClass(
-                  row.status
-                )}`}
-              >
-                {row.status}
-              </span>
-            </td>
-            <td className="py-4 px-6 text-center text-sm cursor-pointer">{row.pitches}</td>
-            <td className="py-4 px-6 text-center text-sm cursor-pointer">{row.postedOn}</td>
+  <>
+    <div className="p-3 my-4 rounded-3xl bg-[#e9eef6] flex items-center w-[60%]">
+      <Search />
+      <input
+        type="text"
+        className="w-full outline-none ml-2 placeholder:text-black"
+        placeholder="Search requests"
+      />
+    </div>
+    <div className="overflow-x-auto my-4 border border-[#e7e0ec] rounded-2xl">
+      <table className="min-w-full bg-white rounded-xl">
+        <thead>
+          <tr>
+            <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c] rounded-tl-xl">
+              Product/Service
+            </th>
+            <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c]">
+              Quantity
+            </th>
+            <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c]">
+              Budget Range
+            </th>
+            <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c]">
+              Status
+            </th>
+            <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c]">
+              Pitches
+            </th>
+            <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c] rounded-tr-xl">
+              Posted On
+            </th>
+            <th className="py-3 px-6 text-center font-bold text-sm text-[#48464c] rounded-tr-xl">
+              Actions
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
+        </thead>
+        <tbody>
+          {data.map((row, idx) => (
+            <tr
+              key={idx}
+              className="border border-gray-100 hover:bg-[#f5f5f5] transition"
+            >
+              <td className="py-4 px-6 text-center text-sm ">
+                {row.productService}
+              </td>
+              <td className="py-4 px-6 text-center text-sm ">{row.quantity}</td>
+              <td className="py-4 px-6 text-center text-sm ">
+                {row.budgetRange}
+              </td>
+              <td className="py-4 px-6 text-center text-sm ">
+                <span
+                  className={`inline-block px-4 py-1 rounded-full text-sm  font-medium ${statusClass(
+                    row.status
+                  )}`}
+                >
+                  {row.status}
+                </span>
+              </td>
+              <td className="py-4 px-6 text-center text-sm ">{row.pitches}</td>
+              <td className="py-4 px-6 text-center text-sm ">{row.postedOn}</td>
+              <td className="py-4 flex items-center justify-center text-sm cursor-pointer">
+                <EllipsisVertical className="text-center" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </>
 );
 
 export default StatusTable;
